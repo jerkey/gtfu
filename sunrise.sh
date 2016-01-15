22b07df69fd3253e7eaf6b797e1ab39d2901c3e9
JayBird <jaybird@debian>
2016-01-15 12:09:52 -0800

    outputs hour and minute

--- sunrise.sh
+++ sunrise.sh
@@ -1,2 +1,3 @@
-cat /tmp/whatever | grep "San Francisco" | cut -d: -f 4 | cut -da -f 1
 curl www.sunrisesunset.com >/tmp/whatever
+cat /tmp/whatever | grep "San Francisco" | cut -d: -f 3
+cat /tmp/whatever | grep "San Francisco" | cut -d: -f 4 | cut -da -f 1
