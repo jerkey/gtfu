5b584c26cb2c1382ea50a992e9695c35defe11b9
jerkey <jerkey@g-cipher.net>
2016-01-17 13:43:31 -0800

    this will store HOUR and MINUTE in variables
    now it will be easy to do the next step:  telling the script to wait until that time.

--- sunrise.sh
+++ sunrise.sh
@@ -1,3 +1,4 @@
 curl www.sunrisesunset.com >/tmp/whatever
-cat /tmp/whatever | grep "San Francisco" | cut -d: -f 3
-cat /tmp/whatever | grep "San Francisco" | cut -d: -f 4 | cut -da -f 1
+HOUR=`cat /tmp/whatever | grep "San Francisco" | cut -d: -f 3`
+MINUTE=`cat /tmp/whatever | grep "San Francisco" | cut -d: -f 4 | cut -da -f 1`
+echo Sunrise will be at $HOUR:$MINUTE AM today (assuming it has not already happened)
